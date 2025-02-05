9a326d5add53b12fd2ca81a4e89a8545b0eeec77
Richard Sherrill <richardzsherrill@gmail.com>
2025-02-05 12:44:02 -0600

    Initial commit

---
+++ src/main/java/com/richardsherrill/website/WebsiteApplication.java
@@ -0,0 +1,29 @@
+package com.richardsherrill.website;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class WebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext context) {
+		return args -> {
+			System.out.println("Inspecting Spring Boot Beans:");
+			String[] beanNames = context.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
+		};
+	}
+}
